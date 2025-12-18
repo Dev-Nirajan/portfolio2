32e80f2d0e87a7b47f3e582598e31fa39b271762
J-Neer <jnirajan050@gmail.com>
2025-12-18 21:24:37 +0545

    change the array position

--- src/Allprojects.js
+++ src/Allprojects.js
@@ -1,28 +1,32 @@
 const projects = [
-  {
-    title: 'Cat Carousel',
-    desc: 'I built this Cat Carousel app while practicing React props and hooks, with a responsive UI styled using Tailwind CSS.',
-    repo: '#',
-    live: '#',
-  },
+
   {
     title: 'Currency converter',
     desc: 'I made this app when I was learning JavaScript. This app use an external API to fetch live currency rates. An appealing currency converter with a beautiful UI.',
     repo: 'https://github.com/Dev-Nirajan/Currency-Converter.git',
     live: 'https://dev-nirajan.github.io/Currency-Converter/',
   },
-  {
-    title: 'Rock Paper Scissor',
-    desc: 'i built this Image Carsoul while learning React fundamentals, focusing on hooks, props, and responsive styling with Tailwind CSS.',
-    repo: '#',
-    live: '#',
-  },
+  
   {
     title: 'Tic Tac Toe',
     desc: 'A Tic Tac Toe game that i build when i was learning events, callbacks, classes and bojects in javasctipt',
     repo: 'https://github.com/Dev-Nirajan/Tic-Tac-Toe',
     live: 'https://dev-nirajan.github.io/Tic-Tac-Toe/',
   },
+  {
+    title: 'Cat Carousel',
+    desc: 'I built this Cat Carousel app while practicing React props and hooks, with a responsive UI styled using Tailwind CSS.',
+    repo: '#',
+    live: '#',
+  },
+
+  {
+    title: 'Rock Paper Scissor',
+    desc: 'i built this Image Carsoul while learning React fundamentals, focusing on hooks, props, and responsive styling with Tailwind CSS.',
+    repo: '#',
+    live: '#',
+  },
+
 
   // {
   //   title: 'Amazon Clone',
